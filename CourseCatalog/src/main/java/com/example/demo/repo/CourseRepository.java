43443ba5343f60390ced3c8de1bb919ef033483d
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2020-09-25 15:45:47 +0600

    Add existing file

--- CourseCatalog/src/main/java/com/example/demo/repo/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repo;
-
-import com.example.demo.models.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-    Iterable<Course> findAllByTitle(String title);
-}
